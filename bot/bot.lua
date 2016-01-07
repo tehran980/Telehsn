32e3fce1d653cdfd8858098daf7980439c60ce2a
tehran980 <hsn.m@torkmail.ir>
2016-01-07 17:08:16 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {93367845},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
